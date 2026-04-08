2afd8b99f0028c2f312bd7efa24fc0ce09741e86
k2hcis03 <k2hcis03@nate.com>
2026-04-08 19:58:42 +0900

    Refactor main.py to use TcpServerThread instead of TcpClientThread; add server.py for TCP server implementation with UnitBoardGetStatus class for handling unit board status updates and communication.

--- source/main.py
+++ source/main.py
@@ -20,7 +20,7 @@ import numpy as np
 import configparser
 import unitboard
 from unitboard import UnitBoard as unit_board
-from client import TcpClientThread as tcp_client
+from server import TcpServerThread as tcp_server
 
 shared_object = Manager().list()            # 프로세스간 공유 소켓 정보
 shared_object.insert(0, None)
@@ -298,9 +298,9 @@ def main():
     main_func.start()
     
     socket_event = threading.Event()
-    main_func.client = tcp_client(tcp_queue, logging, GPIOADDR1, GPIOADDR2, socket_event, i2c_semaphor, MAXUNITBOARD, 
+    main_func.client = tcp_server(tcp_queue, logging, GPIOADDR1, GPIOADDR2, socket_event, i2c_semaphor, MAXUNITBOARD, 
                                   shm.name, main_func.unit_np_shm, socket_send_queue, status_control_queue)
-    main_func.client.start()                            #tcp client 시작
+    main_func.client.start()                            #tcp server 시작
     unitboard.g_file_path = common_config['JSON_FILE']
     # print("Server is not Connected")
     try:
